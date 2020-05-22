0de17fc39694aac4f807f3d21e720e6c2981962a
hardwin welly <hardwin27@gmail.com>
2020-05-22 18:42:34 +0800

    Progress: Create a temporary scrollview that currently only working at web browser, while it totally break the mobile view. Remake the component needed to calculate exp, now with its variable. Funtion to calculate exp is on progress

--- App.js
+++ App.js
@@ -7,7 +7,8 @@ import {
     Picker,
     TextInput,
     ScrollView,
-    Modal
+    Modal,
+    SafeAreaView
 } from 'react-native';
 
 
@@ -15,16 +16,23 @@ class ResourceCalculator extends Component {
     state = {
         resource: "none",
         lmdStage: -1,
-        lmdTargetedValue: 0,
         lmdSanityUsed: 0,
         lmdDropAmount: 0,
+        lmdTargetedValue: 0,
+        lmdTotalSanity: 0,
         lmdTotalRun: 0,
-        lmdResult: 0,
         lmdOverflow: 0,
-        operatorRarity:-1
+        expStage: -1,
+        expSanityUsed: 0,
+        expDropAmount: 0,
+        expOpRarity: -1,
+        expOpCurrentElite: -1,
+        expOpCurrentLevel: 0,
+        expOpTargetedElite: -1,
+        expOpTargetedLevel: 0
     };
 
-    changeCalculationParameter(stageIndex) {
+    changeLmdCalculationParameter(stageIndex) {
         let stageInt = parseInt(stageIndex);
         switch(stageInt){
             case -1:
@@ -36,6 +44,18 @@ class ResourceCalculator extends Component {
         }
     }
 
+    changeExpCalculationParameter(stageIndex) {
+        let stageInt = parseInt(stageIndex);
+        switch(stageInt){
+            case -1:
+                this.setState({expSanityUsed: 0}, this.setState({expDropAmount: 0}));
+                break;
+            case 5:
+                this.setState({expSanityUsed: 30}, this.setState({expDropAmount: 7400}));
+                break;
+        }
+    }
+
     calculateLMD(target, sanity, drop) {
         let runAmount = target / drop;
         let overflow = 0;
@@ -43,87 +63,142 @@ class ResourceCalculator extends Component {
             runAmount = Math.floor(runAmount) + 1;
         }
         overflow = (drop*runAmount) - target;
-        this.setState({lmdResult: runAmount});
-        this.setState({lmdTotalRun: runAmount*sanity});
+        this.setState({lmdTotalRun: runAmount});
+        this.setState({lmdTotalSanity: runAmount*sanity});
         this.setState({lmdOverflow: overflow});
+
+    }
+
+    calculateExp(rarity, currentElite, currentLevel, targetedElite, targetedLevel, sanity, drop) {
+        let startingExpReq = 0;
+        let expDifference = 0;
+        let levelLimit = 0
+        switch(currentElite){
+            case -1:
+                startingExpReq = 0;
+                expDifference = 0;
+                break;
+            case 0:
+                startingExpReq = 100;
+                expDifference = 17;
+                break;
+            case 1:
+                startingExpReq = 120;
+                expDifference = 52;
+                break;
+            case 2:
+                startingExpReq = 191;
+                expDifference = 112;
+                break;
+        };
+        switch(rarity) {
+            case -1:
+                levelLimit = 0;
+                break;
+            case 1:
+            case 2:
+                levelLimit = 30;
+                break;
+            case 3:
+                levelLimit = 40;
+                break;
+            case 4:
+                levelLimit = 45;
+                break;
+            case 5:
+            case 6:
+                levelLimit = 50;
+                break;
+        }
+        if(currentElite <= targetedElite) {
+            if(!(currentElite == targetedElite && currentLevel > targetedLevel)) {
+                console.log("WORKING");
+            }
+        }
 
     }
 
     resourceNone = () => {
         return(
-            <Text>Please select the resource you want to farm.</Text>
+            <Text style={styles.textLeft}>Please select the resource you want to farm.</Text>
         )
     }
 
     resourceExp = () => {
         return(
-          <View style={picker.container}>
-              {/* <View style={picker.underline}>
-                <Picker
-                    style={picker.style}
-                    selectedValue={this.state.stage}
-                    onValueChange={(itemValue, itemIndex) => this.setState({stage: itemValue}, this.changeCalculationParameter(itemValue))}
-                >
-                    <Picker.Item label="Select operator rarity" value={-1}/>
-                    <Picker.Item label="1 Star" value={1}/>
-                    <Picker.Item label="2 Stars" value={2}/>
-                    <Picker.Item label="3 Stars" value={3}/>
-                    <Picker.Item label="4 Stars" value={4}/>
-                    <Picker.Item label="5 Stars" value={5}/>
-                    <Picker.Item label="6 Stars" value={6}/>
-                </Picker>
-              </View>
-              <View style={picker.underline}>
-                <Picker
-                    style={picker.style}
-                    selectedValue={this.state.stage}
-                    onValueChange={(itemValue, itemIndex) => this.setState({stage: itemValue}, this.changeCalculationParameter(itemValue))}
-                >
-                    <Picker.Item label="Select current elite" value={-1}/>
-                    <Picker.Item label="Elite 0" value={0}/>
-                    <Picker.Item label="Elite 1" value={1}/>
-                    <Picker.Item label="Elite 2" value={2}/>
-                </Picker>
-              </View>
-              <TextInput
+            <View style={picker.container}>
+                <View style={picker.underline}>
+                    <Picker
+                        style={picker.style}
+                        selectedValue={this.state.expStage}
+                        onValueChange={(itemValue, itemIndex) => this.setState({expStage: itemValue}, this.changeExpCalculationParameter(itemValue))}
+                    >
+                        <Picker.Item label="Select stage" value={-1}/>
+                        <Picker.Item label="LS-5" value={5}/>
+                    </Picker>
+                </View>
+                <Text style={styles.textLeft}>Your Operator:</Text>
+                <View style={picker.underline}>
+                    <Picker
+                        style={picker.style}
+                        selectedValue={this.state.expOpRarity}
+                        onValueChange={(itemValue, itemIndex) => this.setState({expOpRarity: itemValue})}
+                    >
+                        <Picker.Item label="Select operator rarity" value={-1}/>
+                        <Picker.Item label="1 Star" value={1}/>
+                        <Picker.Item label="2 Stars" value={2}/>
+                        <Picker.Item label="3 Stars" value={3}/>
+                        <Picker.Item label="4 Stars" value={4}/>
+                        <Picker.Item label="5 Stars" value={5}/>
+                        <Picker.Item label="6 Stars" value={6}/>
+                    </Picker>
+                </View>
+                <View style={picker.underline}>
+                    <Picker
+                        style={picker.style}
+                        selectedValue={this.state.expOpCurrentElite}
+                        onValueChange={(itemValue, itemIndex) => this.setState({expOpCurrentElite: itemValue})}
+                    >
+                        <Picker.Item label="Select current elite" value={-1}/>
+                        <Picker.Item label="E0" value={0}/>
+                        <Picker.Item label="E1" value={1}/>
+                        <Picker.Item label="E2" value={2}/>
+                    </Picker>
+                </View>
+                <TextInput
                     style={styles.input}
-                    placeholder="Current level"
+                    placeholder="Current Level"
                     value={this.input}
-                    onChangeText={(input) => this.setState({lmdTargetedValue: input})}
+                    onChangeText={(input) => this.setState({expOpCurrentLevel: input})}
                     keyboardType="numeric"
-              />
-              <View style={picker.underline}>
-                <Picker
-                    style={picker.style}
-                    selectedValue={this.state.stage}
-                    onValueChange={(itemValue, itemIndex) => this.setState({stage: itemValue}, this.changeCalculationParameter(itemValue))}
-                >
-                    <Picker.Item label="Select target elite" value={-1}/>
-                    <Picker.Item label="Elite 0" value={0}/>
-                    <Picker.Item label="Elite 1" value={1}/>
-                    <Picker.Item label="Elite 2" value={2}/>
-                </Picker>
-              </View>
-              <TextInput
+                />
+                <Text> </Text>
+                <Text>Your target:</Text>
+                <View style={picker.underline}>
+                    <Picker
+                        style={picker.style}
+                        selectedValue={this.state.expOpTargetedElite}
+                        onValueChange={(itemValue, itemIndex) => this.setState({expOpTargetedElite: itemValue})}
+                    >
+                        <Picker.Item label="Select targeted elite" value={-1}/>
+                        <Picker.Item label="E0" value={0}/>
+                        <Picker.Item label="E1" value={1}/>
+                        <Picker.Item label="E2" value={2}/>
+                    </Picker>
+                </View>
+                <TextInput
                     style={styles.input}
-                    placeholder="Target level"
+                    placeholder="Targeted Level"
                     value={this.input}
-                    onChangeText={(input) => this.setState({lmdTargetedValue: input})}
+                    onChangeText={(input) => this.setState({expOpTargetedLevel: input})}
                     keyboardType="numeric"
-              />
-              <View style={picker.underline}>
-                <Picker
-                    style={picker.style}
-                    selectedValue={this.state.stage}
-                    onValueChange={(itemValue, itemIndex) => this.setState({stage: itemValue}, this.changeCalculationParameter(itemValue))}
+                />
+                <TouchableOpacity
+                    onPress={() => this.calculateExp(parseInt(this.state.expOpRarity), parseInt(this.state.expOpCurrentElite), parseInt(this.state.expOpCurrentLevel), parseInt(this.state.expOpTargetedElite), parseInt(this.state.expOpTargetedLevel), parseInt(this.state.expSanityUsed), parseInt(this.state.expDropAmount))}
+                    style={styles.button}
                 >
-                    <Picker.Item label="Select stage" value={-1}/>
-                    <Picker.Item label="LS-5" value={5}/>
-                </Picker>
-              </View> */}
-              <Text style={styles.textRequire}>Require:</Text>
-              <Text style={styles.textLeft}>{parseInt(this.state.lmdResult)} run</Text>
-              <Text style={styles.textLeft}>{parseInt(this.state.lmdResult)*30} sanity</Text>
+                    <Text style={styles.buttonText}>Calculate</Text>
+                </TouchableOpacity>
             </View>
         )
     }
@@ -136,7 +211,7 @@ class ResourceCalculator extends Component {
                 <Picker
                     style={picker.style}
                     selectedValue={this.state.lmdStage}
-                    onValueChange={(itemValue, itemIndex) => this.setState({lmdStage: itemValue}, this.changeCalculationParameter(itemValue))}
+                    onValueChange={(itemValue, itemIndex) => this.setState({lmdStage: itemValue}, this.changeLmdCalculationParameter(itemValue))}
                 >
                     <Picker.Item label="Select stage" value={-1}/>
                     <Picker.Item label="CE-5" value={5}/>
@@ -156,8 +231,8 @@ class ResourceCalculator extends Component {
                   <Text style={styles.buttonText}>Calculate</Text>
               </TouchableOpacity>
               <Text style={styles.textRequire}>Require:</Text>
-              <Text style={styles.textLeft}>{parseInt(this.state.lmdResult)} run</Text>
-              <Text style={styles.textLeft}>{parseInt(this.state.lmdTotalRun)} sanity</Text>
+              <Text style={styles.textLeft}>{parseInt(this.state.lmdTotalRun)} run</Text>
+              <Text style={styles.textLeft}>{parseInt(this.state.lmdTotalSanity)} sanity</Text>
               <Text style={styles.textLeft}> </Text>
               <Text style={styles.textLeft}>You will get {parseInt(this.state.lmdOverflow)} extra LMD</Text>
             </View>
@@ -186,22 +261,27 @@ class ResourceCalculator extends Component {
         return(
           // <ScrollView style={{backgroundColor:'#123456',height:'auto'}}>
             <View style={styles.container}>
-                <Text style={styles.header}>Resource Calculator</Text>
-                <View style={picker.container}>
-                  <View style={picker.underline}>
-                    <Picker
-                      style={picker.style}
-                      itemStyle={picker.itemStyle}
-                      selectedValue={this.state.resource}
-                      onValueChange={(itemValue, itemIndex) => this.setState({resource: itemValue})}
-                    >
-                      <Picker.Item label="Select resource" value={null}/>
-                      <Picker.Item label="EXP" value="exp"/>
-                      <Picker.Item label="LMD" value="lmd"/>
-                      </Picker>
-                  </View>
-                  {this.checkSelectedResource(this.state.resource)}
-                </View>
+                <ScrollView>
+                    <SafeAreaView>
+                    <Text style={styles.header}>Resource Calculator</Text>
+                    <View style={picker.container}>
+                      <View style={picker.underline}>
+                        <Picker
+                          style={picker.style}
+                          itemStyle={picker.itemStyle}
+                          selectedValue={this.state.resource}
+                          onValueChange={(itemValue, itemIndex) => this.setState({resource: itemValue})}
+                        >
+                          <Picker.Item label="Select resource" value={null}/>
+                          <Picker.Item label="EXP" value="exp"/>
+                          <Picker.Item label="LMD" value="lmd"/>
+                          </Picker>
+                      </View>
+                      {this.checkSelectedResource(this.state.resource)}
+                    </View>  
+                    </SafeAreaView>
+                </ScrollView>
+                
             </View>
           // </ScrollView>
         )
