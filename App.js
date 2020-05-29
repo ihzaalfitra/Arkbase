5caf47ddfd83c32fc31ffbd3a58d4eac265f1b2a
ihzaalfitra <ihzaalfitra@gmail.com>
2020-05-29 18:27:55 +0700

    adding navigation pannel

--- App.js
+++ App.js
@@ -13,14 +13,83 @@ import {
 } from 'react-native';
 
 import ResourceCalculator from './resourceCalculator.js';
+import navigation from './assets/stylesheet/navigation.js';
 
 const contentHeight=Math.round(Dimensions.get('window').height)-100;
 
 class MainClass extends Component {
+	state = {
+		navigationContent:'',
+    }
+	changeContent(selectedContent){
+		switch(selectedContent){
+			case 'ResourceCalculator':
+				return(
+					<ResourceCalculator/>
+				)
+				break;
+			case 'StageInformation':
+				return(
+					<View>
+						<Text>Stage Information</Text>
+					</View>
+				)
+				break;
+			default:
+				return(
+					<ResourceCalculator/>
+				)
+				break;
+		}
+	}
 	render(){
 		return(
-			<ResourceCalculator>
-			</ResourceCalculator>
+			<View style={{flex:1}}>
+				{
+					this.changeContent(this.state.navigationContent)
+				}
+				<View style={navigation.container}>
+					<View style={{flexDirection:'row'}}>
+						<TouchableOpacity
+							style={navigation.button}
+							onPress={() => this.changeContent('ResourceCalculator')}
+						>
+							<Text style={navigation.text}>
+								Resource Calculator
+							</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity
+							style={navigation.button}
+							onPress={() => this.changeContent('StageInformation')}
+						>
+							<Text style={navigation.text}>
+								Stage Information
+							</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity
+							style={navigation.button}
+							onPress={() => this.changeContent('Operator')}
+						>
+							<Text style={navigation.text}>
+								Operator
+							</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity
+							style={navigation.button}
+							onPress={() => this.changeContent('About')}
+						>
+							<Text style={navigation.text}>
+								About
+							</Text>
+						</TouchableOpacity>
+
+					</View>
+				</View>
+			</View>
+
 		)
 	}
 }
---
+++ assets/stylesheet/navigation.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const navigation = StyleSheet.create({
+	container:{
+		flex:1,
+		backgroundColor:'#000'
+	},
+	text:{
+		textAlign:'center',
+		color:'#fff'
+	},
+	button:{
+		flex:1,
+		height:'120%',
+		borderWidth:1,
+		backgroundColor:'#222',
+		borderColor:'#333',
+		justifyContent:'center'
+	}
+
+});
+
+export default navigation;
