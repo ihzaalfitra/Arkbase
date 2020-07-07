7fafe93db79d84a3f3cab2f20eeb0e69b6b7852e
ihzaalfitra <ihzaalfitra@gmail.com>
2020-07-07 21:18:32 +0700

    materials title, scroll, and navigation

--- Component/Material.js
+++ Component/Material.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
-import {StyleSheet, Text, View, FlatList, Dimensions } from 'react-native'
+import {StyleSheet, Text, View, FlatList, Dimensions, ScrollView } from 'react-native'
 
+import default_styles from '../assets/Stylesheet/styles.js';
 
 const dataList = [{key: '1'}, {key: '2'}, {key: '3'}, {key: '4'}, {key: '5'},{key: '6'}, {key: '7'}, {key: '8'}, {key: '9'}, {key: '10'},{key: '11'}, {key: '12'}, {key: '13'}, {key: '14'}, {key: '15'}
 ,{key: '16'}, {key: '17'}, {key: '18'}, {key: '19'}, {key: '20'},{key: '21'}, {key: '22'}, {key: '23'}, {key: '24'}, {key: '25'},{key: '26'}, {key: '27'}, {key: '28'}, {key: '29'}, {key: '30'}]
@@ -12,7 +13,7 @@ const WIDTH =  Dimensions.get('window').width
 
 export default class App extends Component {
 
-    
+
     formatData = (dataList, numColumns) => {
       const totalRows = Math.floor(dataList.length / numColumns)
       let totalLastRows = dataList.length - (totalRows * numColumns)
@@ -30,7 +31,7 @@ export default class App extends Component {
         return <View style = {[itemStyle, itemInvisible]}/>
       }
       return (
-        
+
         <View style = {itemStyle}>
           <Text style = {itemText}>{item.key}</Text>
         </View>
@@ -39,20 +40,20 @@ export default class App extends Component {
 
     render(){
       let {container, itemText} = styles
-      
+
       return (
-        
-        <View style = {{container,backgroundColor:'#000',paddingTop: 150}}>
-         
-          <FlatList
-          
-          data = {this.formatData(dataList, numColumns)}
-          renderItem = {this._renderItem}
-          keyExtractor = {(item, index) => index.toString()}
-          numColumns = {numColumns}
-        
-          />
-          
+        <View style = {{container,backgroundColor:'#000',paddingTop: 25,flex:9}}>
+			<ScrollView>
+				<Text style={default_styles.header}>Materials
+				</Text>
+		        <FlatList
+					data = {this.formatData(dataList, numColumns)}
+		          	renderItem = {this._renderItem}
+		          	keyExtractor = {(item, index) => index.toString()}
+		          	numColumns = {numColumns}
+				/>
+		  </ScrollView>
+
         </View>
       )
     }
@@ -65,8 +66,8 @@ const styles = StyleSheet.create(
   container: {
     flex:1,
     paddingTop : 40,
-    
-   
+
+
   },
   itemStyle:{
     backgroundColor: '#000',
@@ -81,7 +82,7 @@ const styles = StyleSheet.create(
     marginVertical: 20,
     borderColor: '#fff',
     height: WIDTH/ numColumns,
- 
+
 
   },
   itemText: {
