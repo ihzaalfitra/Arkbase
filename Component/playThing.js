081f7cc142dd63de0b0892b1c9a9d63f7180b22e
ihzaalfitra <ihzaalfitra@gmail.com>
2020-07-18 19:19:06 +0700

    for some reason updating firebase again, commits plaything as experimental file for orientation

---
+++ Component/playThing.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {
+    Text,
+    View,
+    Picker,
+    ScrollView,
+	Dimensions
+} from 'react-native';
+import styles from '..//assets/Stylesheet/styles.js';
+import picker from '../assets/Stylesheet/picker.js';
+
+class ResourceCalculator extends Component {
+	isPortrait(){
+		const dim = Dimensions.get('screen');
+		return dim.height>=dim.width;
+	}
+    state = {
+		orientation:this.isPortrait()?'portraite':'landscape'
+    };
+
+	Dimensions.addEventListener('change', () => {
+        this.setState({orientation: this.isPortrait() ? 'portraite' : 'landscape'});
+    });
+
+	render() {
+
+        return(
+			<View style={{flex:9,backgroundColor:'#000',paddingTop:25}}>
+				<Text style={styles.textLeft}>orientation is :{this.state.orientation}</Text>
+			</View>
+        )
+    }
+}
+
+export default ResourceCalculator;
